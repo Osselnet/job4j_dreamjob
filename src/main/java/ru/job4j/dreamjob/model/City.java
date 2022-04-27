741a36ea613c5012bf8058884143c4cc0d547a57
Oleg Sergienko <hornets@list.ru>
2022-04-27 14:01:57 +0300

    2. Формы. Списки.

--- src/main/java/ru/job4j/dreamjob/model/City.java
+++ src/main/java/ru/job4j/dreamjob/model/City.java
@@ -33,8 +33,12 @@ public class City implements Serializable {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
         City city = (City) o;
         return id == city.id;
     }
